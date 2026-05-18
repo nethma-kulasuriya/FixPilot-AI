205b9154e7c531453ee6257f07ecddb22f1263e4
nethma-kulasuriya <nethmamkulasuriya@gmail.com>
2026-05-18 20:39:05 +0530

    Created persistent database and stored AI results

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
+from database import engine, SessionLocal
+from models import TicketDB, Base
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-# Load models
+# Load ML models
 category_model = joblib.load("category_model.pkl")
 priority_model = joblib.load("priority_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
@@ -13,16 +19,36 @@ vectorizer = joblib.load("vectorizer.pkl")
 class Ticket(BaseModel):
     issue: str
 
-# Prediction endpoint
 @app.post("/predict")
 def predict(ticket: Ticket):
 
+    # Convert issue text
     X = vectorizer.transform([ticket.issue])
 
-    category_prediction = category_model.predict(X)
-    priority_prediction = priority_model.predict(X)
+    # Predictions
+    category_prediction = category_model.predict(X)[0]
+    priority_prediction = priority_model.predict(X)[0]
+
+    # Database session
+    db = SessionLocal()
+
+    # Create ticket record
+    new_ticket = TicketDB(
+        issue=ticket.issue,
+        category=category_prediction,
+        priority=priority_prediction
+    )
+
+    # Save to database
+    db.add(new_ticket)
+    db.commit()
+    db.refresh(new_ticket)
+
+    db.close()
 
     return {
-        "category": category_prediction[0],
-        "priority": priority_prediction[0]
+        "id": new_ticket.id,
+        "issue": new_ticket.issue,
+        "category": new_ticket.category,
+        "priority": new_ticket.priority
     }
